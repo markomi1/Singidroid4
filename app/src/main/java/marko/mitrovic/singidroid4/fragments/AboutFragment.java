ecb63fbcc21632f16817d133324a986229dd366b
Marko Mitrovic <okram.je.car.4@gmail.com>
2021-06-11 00:51:54 +0200

    Added oss-licenses plugin, fragment_about.xml layout is 90% done.

--- app/src/main/java/marko/mitrovic/singidroid4/fragments/AboutFragment.java
+++ app/src/main/java/marko/mitrovic/singidroid4/fragments/AboutFragment.java
@@ -1,13 +1,16 @@
 package marko.mitrovic.singidroid4.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import marko.mitrovic.singidroid4.R;
 
 public class AboutFragment extends Fragment {
@@ -16,11 +19,14 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_about,container,false);
+        view = inflater.inflate(R.layout.fragment_about, container, false);
 
+        TextView licenseInfo = view.findViewById(R.id.ossLicense);
 
+        licenseInfo.setOnClickListener(v -> {
+            startActivity(new Intent(this.getActivity(), OssLicensesMenuActivity.class));
+        });
 
-        //((MainActivity) getActivity()).setStatusBarColor(0, 0, "#008A94", false);
         return view;
     }
 }
